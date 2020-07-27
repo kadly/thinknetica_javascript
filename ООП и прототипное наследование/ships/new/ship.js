7d583d93a32e9ecc22e7e2869d081571ee98af80
Dmitry <d111itry@gmail.com>
2020-07-28 00:54:40 +0700

    rerun-3

--- ООП и прототипное наследование/ships/new/ship.js
+++ ООП и прототипное наследование/ships/new/ship.js
@@ -2,15 +2,31 @@
 
 //корабль
 function Ship(name, model, color) {
-
+  this.name = name;
+  this.model = model;
+  this.color = color;
 }
 
 //моторный корабль
 function MotorShip(parameters) {
-
+  this.name = parameters.name;
+  this.model = parameters.model;
+  this.color = parameters.color;
+  this.motorPerformance = parameters.motorPerformance;
+  this.bodyMaterial = parameters.bodyMaterial;
 }
 
 //парусный корабль
 function SailingShip(parameters) {
-
+  this.name = parameters.name;
+  this.model = parameters.model;
+  this.color = parameters.color;
+  this.mastNumber = parameters.mastNumber;
+  this.sailArea = parameters.sailArea;
 }
+
+MotorShip.prototype = Object.create(Ship.prototype);
+MotorShip.prototype.constructor = MotorShip;
+
+SailingShip.prototype = Object.create(Ship.prototype);
+SailingShip.prototype.constructor = SailingShip;
