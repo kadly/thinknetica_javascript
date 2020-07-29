b84e19bbc7dd51e41173e275c3375a9c99605a3d
Dmitry <d111itry@gmail.com>
2020-07-29 15:47:14 +0700

    rerun-4

--- ООП и прототипное наследование/ships/new/wharf.js
+++ ООП и прототипное наследование/ships/new/wharf.js
@@ -2,12 +2,26 @@
 
 //обменять корабль
 function SwapShip(typeOfShip, name) {
-  
+  здесь кроме аргументов, указанных выше, нужны ещё MSFIDOCredentialAssertion, color, motorPerformance, bodyMaterial, mastNumber, sailArea
+  не понимаю, как это сделать
 }
 
 //покрасить корабль
-function DyeShip(name) {
-  
+function DyeShip(name, color) {
+  switch (color) {
+    case 'b':
+      this.color = 'black';
+      let theShipWasPainted = 1;
+      console.log(`${ship.name} has been successfully colored to Black`);
+      break;
+    case 'w':
+      this.color = 'white';
+      let theShipWasPainted = 1;
+      console.log(`${ship.name} has been successfully colored to White`);
+      break;
+    default:
+      throw new Error(`Invalid color "${this.color}"`);
+  }
 }
 
 //отремонтировать корабль
